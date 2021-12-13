d70cc183fb3b64b5cf7566757a7ae5d27ebadfce
hamid-2117 <hamid@med-examexpert.com>
2021-12-13 16:00:44 +0500

    added locally image upload in draft js

--- component/Draft-js/index.js
+++ component/Draft-js/index.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react"
 import { Editor } from "react-draft-wysiwyg"
-import { EditorState, convertToRaw } from "draft-js"
-
+import { EditorState, convertToRaw, ContentState } from "draft-js"
+import htmlToDraft from "html-to-draftjs"
+import { Button, TextField } from "@material-ui/core"
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
 import draftToHtml from "draftjs-to-html"
-
+import axios from "axios"
 export default class TextEditor extends Component {
   state = {
     editorState: EditorState.createEmpty(),
+    name: "",
+    subject: "",
+    toUpdateName: "",
+  }
+  componentDidMount() {
+    if (!!this.props.name) {
+      this.setState({ toUpdateName: this.props.name })
+      axios
+        .get(
+          `${process.env.REACT_APP_BackendURL}/general/getEmailTemplate/${this.props.name}`,
+          {
+            withCredentials: true,
+          }
+        )
+        .then(({ data }) => {
+          let blocksFromHtml = htmlToDraft(data.html)
+          const { contentBlocks, entityMap } = blocksFromHtml
+          const contentState = ContentState.createFromBlockArray(
+            contentBlocks,
+            entityMap
+          )
+          const editorState = EditorState.createWithContent(contentState)
+          this.setState({
+            name: data.name.trim(),
+            subject: data.subject.trim(),
+            editorState,
+          })
+        })
+    }
   }
 
   onEditorStateChange = (editorState) => {
@@ -15,24 +45,111 @@ export default class TextEditor extends Component {
       editorState,
     })
   }
+  handleSubjectChange = (e) => {
+    this.setState({ subject: e.target.value })
+  }
+  handleNameChange = (e) => {
+    this.setState({ name: e.target.value })
+  }
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    if (this.props.type === 1) {
+      await axios.post(
+        `${process.env.REACT_APP_BackendURL}/general/createEmailTemplate`,
+        {
+          name: this.state.name.trim(),
+          subject: this.state.subject.trim(),
+          html: JSON.stringify(
+            draftToHtml(
+              convertToRaw(this.state.editorState.getCurrentContent())
+            )
+          ),
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      this.props.close()
+    } else {
+      await axios.patch(
+        `${process.env.REACT_APP_BackendURL}/general/updateEmailTemplate/${this.state.toUpdateName}`,
+        {
+          newName: this.state.name,
+          subject: this.state.subject,
+          html: JSON.stringify(
+            draftToHtml(
+              convertToRaw(this.state.editorState.getCurrentContent())
+            )
+          ),
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      this.props.close()
+    }
+  }
+  uploadFile = async (data) => {
+    return await axios.post(
+      `${process.env.REACT_APP_BackendURL}/gcp/uploadPQPhoto`,
+      data
+    )
+  }
+  uploadCallback = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new window.FileReader()
+      reader.onloadend = async () => {
+        const form_data = new FormData()
+        form_data.append("file", file)
+        const res = await this.uploadFile(form_data)
+        resolve({ data: { link: res.data } })
+      }
+      reader.readAsDataURL(file)
+    })
+  }
 
   render() {
     const { editorState } = this.state
-    console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())))
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
+        <div style={{ display: "grid" }}>
+          <TextField
+            label="Name"
+            variant="standard"
+            required
+            value={this.state.name}
+            style={{ width: "60%", marginBottom: "10px" }}
+            onChange={this.handleNameChange}
+          />
+          <TextField
+            value={this.state.subject}
+            onChange={this.handleSubjectChange}
+            label="Subject"
+            required
+            style={{ width: "60%", marginBottom: "30px" }}
+            variant="standard"
+          />
+        </div>
         <Editor
           editorState={editorState}
+          toolbar={{
+            image: {
+              uploadCallback: this.uploadCallback,
+              previewImage: true,
+              alt: { present: true, mandatory: false },
+              inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg",
+            },
+          }}
           toolbarClassName="toolbarClassName"
           wrapperClassName="wrapperClassName"
           editorClassName="editorClassName"
+          placeholder="Start typing ......."
           onEditorStateChange={this.onEditorStateChange}
         />
-        <textarea
-          disabled
-          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-        ></textarea>
-      </div>
+        <Button type="submit" style={{ marginTop: "30px" }}>
+          {this.props.type === 1 ? "create" : "edit"}
+        </Button>
+      </form>
     )
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -3,50 +3,29 @@ import axios from "axios"
 import Loader from "../component/loader"
 export default function Home() {
   const [parcent, setParcent] = useState(0)
-  const [error, setError] = useState("")
   const uploadFile = async ({ target: { files } }) => {
-    if (
-      files[0]?.type === "image/png" ||
-      files[0]?.type === "image/jpeg" ||
-      files[0]?.type === "image/png" ||
-      files[0]?.type === "video/mp4" ||
-      files[0]?.type === "video/avi" ||
-      files[0]?.type === "video/avi" ||
-      files[0]?.type === "video/mkv" ||
-      files[0]?.type === "application/pdf" ||
-      files[0]?.type === "application/vnd.ms-excel" ||
-      files[0]?.name.substr(-5) === ".docx"
-    ) {
-      setError("")
+    let data = new FormData()
+    data.append("images", files[0])
+    const options = {
+      onUploadProgress: (progressEvent) => {
+        const { loaded, total } = progressEvent
 
-      let data = new FormData()
-      data.append("images", files[0])
-      const options = {
-        onUploadProgress: (progressEvent) => {
-          const { loaded, total } = progressEvent
-
-          let percent = Math.floor((loaded * 100) / total)
-          setParcent(percent)
-        },
-      }
-      const res = await axios.post(
-        `https://guardaround.herokuapp.com/api/v1/incidentimg`,
-        data,
-        options
-      )
-    } else {
-      setError(
-        "Please Upload on of these file : image/png, image/jpeg, image/png, video/mp4, video/avi, video/avi, video/mkv, application/pdf, application/vnd.ms-excel, .docx"
-      )
+        let percent = Math.floor((loaded * 100) / total)
+        setParcent(percent)
+      },
     }
+    const res = await axios.post(
+      `https://guardaround.herokuapp.com/api/v1/incidentimg`,
+      data,
+      options
+    )
   }
 
   return (
     <>
-      {error && <h4>{error}</h4>}
       <input
         type="file"
-        // accept="image/png, image/jpeg, image/png, video/mp4, video/avi, video/avi, video/mkv, application/pdf, application/vnd.ms-excel, .docx"
+        accept="image/png, image/jpeg, image/png, video/mp4, video/avi, video/avi, video/mkv, application/pdf, application/vnd.ms-excel, .docx"
         onChange={uploadFile}
       />
       <Loader parcent={parcent} />
